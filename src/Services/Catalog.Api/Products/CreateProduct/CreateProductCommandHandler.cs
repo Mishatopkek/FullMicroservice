2d335a3e06b973744754133b15475325f43115bd
Mykhailo Bezuhlyi <bezuhlyi.mykhailo@gmail.com>
2024-12-25 17:06:36 +0200

    Add Create Product

---
+++ src/Services/Catalog.Api/Products/CreateProduct/CreateProductCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using Catalog.Api.Models;
+
+namespace Catalog.Api.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    : ICommand<CreateProductResponse>;
+public record CreateProductResponse(Guid Id);
+
+internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResponse>
+{
+    public async Task<CreateProductResponse> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        var product = new Product()
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
+
+        return new CreateProductResponse(Guid.NewGuid());
+    }
+}
