15361b91f77e5f9fc43d0730747abab64a889039
Mykhailo Bezuhlyi <bezuhlyi.mykhailo@gmail.com>
2024-12-25 17:35:04 +0200

    Add ProductNotFoundException and GetProducts endpoint with a query handler

---
+++ src/Services/Catalog.Api/Products/GetProducts/GetProductsQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.Api.Models;
+
+namespace Catalog.Api.Products.GetProducts;
+
+public record GetProductsQuery() : IQuery<GetProductsResult>;
+
+public record GetProductsResult(IEnumerable<Product> Products);
+
+public class GetProductsQueryHandler(IDocumentSession session, ILogger<GetProductsQueryHandler> logger) : IQueryHandler<GetProductsQuery, GetProductsResult>
+{
+    public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetProducts called {@Query}", query);
+
+        IReadOnlyList<Product> products = await session.Query<Product>().ToListAsync(cancellationToken);
+
+        return new GetProductsResult(products);
+    }
+}
