dff809388011ca978fcdeae636baf38aee59e7fb
Mykhailo Bezuhlyi <bezuhlyi.mykhailo@gmail.com>
2025-01-06 16:32:44 +0200

    Add GetProduct endpoint and a query for it

---
+++ src/Services/Catalog.Api/Products/GetProductById/GetProductByQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using Catalog.Api.Exceptions;
+using Catalog.Api.Models;
+
+namespace Catalog.Api.Products.GetProductById;
+
+public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+
+public record GetProductByIdResult(Product Product);
+
+public class GetProductByQueryHandler(IDocumentSession session, ILogger<GetProductByQueryHandler> logger) : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+{
+    public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetProductByIdQuery called with {@Query}", query);
+
+        var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
+
+        if (product is null)
+        {
+            throw new ProductNotFoundException();
+        }
+
+        return new GetProductByIdResult(product);
+    }
+}
