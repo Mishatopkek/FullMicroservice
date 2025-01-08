465d9c1b771b36e51dd30ddf6273f52da0851b45
Mykhailo Bezuhlyi <bezuhlyi.mykhailo@gmail.com>
2025-01-08 19:26:41 +0200

    Add middleware for exceptions

--- src/Services/Catalog.Api/Program.cs
+++ src/Services/Catalog.Api/Program.cs
@@ -1,5 +1,7 @@
 using System.Reflection;
 using BuildingBlocks.Behaviours;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 
 Assembly? catalogApiAssembly = typeof(Program).Assembly;
 
@@ -23,5 +25,28 @@ builder.Services.AddMarten(option =>
 var app = builder.Build();
 
 app.MapCarter();
+
+app.UseExceptionHandler(exceptionHandlerApp =>
+{
+    exceptionHandlerApp.Run(async context =>
+    {
+        Exception? exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+        if (exception == null) return;
+
+        ProblemDetails problemDetails = new()
+        {
+            Status = StatusCodes.Status500InternalServerError,
+            Title = exception.Message,
+            Detail = exception.StackTrace
+        };
+
+        ILogger<Program> logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+        logger.LogError(exception, exception.Message);
+
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Response.ContentType = "application/problem+json";
+        await context.Response.WriteAsJsonAsync(problemDetails);
+    });
+});
 
 await app.RunAsync();
